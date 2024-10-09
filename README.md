a69ed7ea0691fbf9c60cefd294fc47b2832ca57c
Jimmy García <jimmyegc@gmail.com>
2024-10-08 22:42:15 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#
+https://codesandbox.io/p/sandbox/react-datepicker-issue-wyunxm?file=%2Fsrc%2Findex.tsx%3A14%2C6
---
+++ src/useDebounce.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+export const useDebounce = <T>(value: T, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+/*import { useState, useEffect } from 'react'
+
+export const useDebounce = (value: any, milliSeconds: number) => {
+ const [debouncedValue, setDebouncedValue] = useState(value);
+
+ useEffect(() => {
+   const handler = setTimeout(() => {
+     setDebouncedValue(value);
+   }, milliSeconds);
+
+   return () => {
+     clearTimeout(handler);
+   };
+ }, [value, milliSeconds]);
+
+ return debouncedValue;
+};
+*/
